627f05a7c8f61d00332ea40edc9fc89369c3fcaa
ErnestoDanielTafoyaMolina <edaniimol@gmail.com>
2023-10-10 17:39:01 -0600

    correcion de rutas
    he corregido la ruta que estaba mal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { ProductsComponent } from './pages/products/products.component';
+import { ChatComponent } from './pages/chat/chat.component';
 
 const routes: Routes = [
+  { path:'', component: MenuComponent },
   { path:'login', component: LoginComponent },
   { path:'register', component: RegisterComponent },
-  { path:'menu', component: MenuComponent },
-  { path:'products', component: ProductsComponent }
+  { path:'products', component: ProductsComponent },
+  { path:'chat', component: ChatComponent },
 
 ];
 
